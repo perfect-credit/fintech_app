b95b2cb0b8fdd95b755c442a697f265b17510604
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2019-11-23 18:27:42 +0100

    Proper data dict Quantity, product_name, price

--- api/algorithms.py
+++ api/algorithms.py
@@ -2,15 +2,20 @@ from api.utils import *
 
 # Products extractor
 def extract_receipt_product(filename):
-    product_dict = {}
+    products_dict = []
     for line in image_parser(filename).split("\n"):
         if any(c.isupper() for c in list(line)) and all([word.isupper() for word in list(filter(lambda x: not any(y.isdigit() for y in x), line.split()))]):
+            product = {}
             product_name = []
             for (i, word) in enumerate(line.split()):
-                if word.isupper(): product_name.append(word)
-                elif i > 0: 
-                    products_price = word
+                if i == 0 and word.isdigit(): quantity = int(word)
+                elif word.isupper(): product_name.append(word)
+                else: 
+                    product_price = float("".join(list(map(lambda x: x if x != ',' else '.', list(word)))))
                     break
             product_name = " ".join(product_name)
-            product_dict[product_name] = products_price
-    return product_dict
+            product["quantity"] = quantity
+            product["product_name"] = product_name
+            product["price"] = product_price
+            products_dict.append(product)
+    return products_dict
